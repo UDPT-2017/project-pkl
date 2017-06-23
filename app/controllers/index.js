42a207de408ccc77de3536433d0c35fc1991fbac
lam0196vn <lam0196vn@gmail.com>
2017-06-23 17:09:25 +0700

    Thêm giới thiệu

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   home: require('./home_controller'),
   signUp: require('./sign_up_controller'),
-  signIn: require('./sign_in_controller')
+  signIn: require('./sign_in_controller'),
+  about:  require('./about_controller')
 };
--- config/routes.js
+++ config/routes.js
@@ -9,14 +9,17 @@ module.exports = function(app, passport) {
         .post('/', passport.authenticate('local_signIn', {
                 successRedirect: '/',
                 failureRedirect: '/',
-                failureFlash: true 
+                failureFlash: true
             }))
         .get('/signOut', controllers.signIn.index);
 
     var signUp = router()
       .get('/', controllers.signUp.index)
       .post('/', controllers.signUp.signUp);
+    var about = router()
+      .get('/',controllers.about.index);
 
+    app.use('/about',about);
     app.use('/', home);
     app.use('/signIn', signIn);
     app.use('/signUp', signUp);
